c5e5b25a2749ce92ddfa87a7578a6644d61b951b
Sam Pohlenz <sam@sampohlenz.com>
2013-05-24 10:13:02 +0930

    New Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WheelhouseSearchDemo::Application.config.secret_token = 'ffd4cdab3f9e9687ed16d6053f6c11cfa5835e32976d0d9b95dedc32d5d7a51370ca9967a7373546134193e6953be585da8085878c0edf785aa895a46e4c74d6'
